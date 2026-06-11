64c607723629a2c03bb344afbc261b11830cdbba
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-06-11 20:35:10 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -34,7 +34,7 @@ cd /app
 unzip /tmp/catalogue.zip
 npm install 
 
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp $current/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 
 systemctl enable catalogue 
